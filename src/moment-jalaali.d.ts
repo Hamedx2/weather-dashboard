d50eb5be314686da53c1113c4548103fd067a952
Hamed Teymouri <html113343@gmail.com>
2025-05-20 19:41:02 +0330

    fixing a bug and changing the site name and icon

--- src/moment-jalaali.d.ts
+++ src/moment-jalaali.d.ts
@@ -1,4 +1,18 @@
 declare module "moment-jalaali" {
   import * as moment from "moment";
-  export = moment;
+
+  interface MomentJalaali extends moment.Moment {
+    format(format: string): string;
+  }
+
+  interface MomentStatic extends moment.MomentStatic {
+    loadPersian(config?: {
+      dialect?: "persian" | "persian-modern";
+      usePersianDigits?: boolean;
+    }): void;
+    (): MomentJalaali;
+  }
+
+  const momentJalaali: MomentStatic;
+  export = momentJalaali;
 }
